d0ad89531cf5eefdc2685076217cd553c27b112f
joaquin solis <sol19008@byui.edu>
2023-08-21 13:54:12 -0300

    fix format numbers

--- client/src/components/priceNumber/index.jsx
+++ client/src/components/priceNumber/index.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import styles from "./priceNumber.module.css";
 
 const PriceNumber = ({ price, className }) => {
-  const [integer, decimal] = price.split(".");
+  const priceString = price.toString();
+  const [integer, decimal] = priceString.split(".");
+
+  const integerWithSeparator = integer.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
   return (
     <div className={className}>
-      {integer}
+      $ {integerWithSeparator}
       <span className={styles.itemDecimals}>{decimal}</span>
     </div>
   );
